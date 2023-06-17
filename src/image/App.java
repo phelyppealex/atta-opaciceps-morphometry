94277460ad7da6771aecfdafe93dc040d3f02786
Phelyppe Álex <phelyppelex@pop-os.localdomain>
2023-06-17 01:37:12 -0300

    cropageam da pilha para contagem de colunas

--- src/image/App.java
+++ src/image/App.java
@@ -35,7 +35,36 @@ public class App {
             11
         );
 
+        /*
+         * Criando um vetor booleano do tamanho da largura
+         * da imagem para que vejamos quais colunas possuem
+         * pixels de foreground.
+         */
+        var somaPixels = new boolean[imLogica[0].length];
+        for(int i = 0; i < imLogica.length; i++)
+            for(int j = 0; j < imLogica[0].length; j++)
+                if(imLogica[i][j])
+                    somaPixels[j] = true;
         
-        // Image.imWrite(imLogica, pathResult+"test2.jpg");
+        int inicioPilha = -1, fimPilha = -1;
+        for(int i = somaPixels.length-1; i >= 0; i--){
+            if(somaPixels[i]){
+                if(inicioPilha == -1)
+                    inicioPilha = i;
+                if(somaPixels[i-1] == false){
+                    fimPilha = i;
+                    break;
+                }
+            }
+        }
+
+        var pilha = new boolean[imLogica.length][inicioPilha-fimPilha+1];
+
+        for(int i = 0; i < pilha.length; i++)
+            for(int j = 0; j < pilha[0].length; j++)
+                pilha[i][j] = imLogica[i][j+fimPilha];
+        
+        Image.imWrite(pilha, pathResult+"pilha.jpg");
+        //Image.imWrite(imLogica, pathResult+"teste2.jpg");
     }
 }
